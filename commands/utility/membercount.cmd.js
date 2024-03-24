382c58d6ce3e071fbfa8eac650e2b4544bee88ce
Brncray <57870234+Brncray@users.noreply.github.com>
2024-03-24 14:13:31 -0400

    membercount

---
+++ commands/utility/membercount.cmd.js
@@ -0,0 +1,28 @@
+/**@type {import("../bot.js").Command} */
+export const data = {
+  name: "membercount",
+  type: 1, // u got 3 types, 1 is reg cmd, 2 is msg app, 3 is user app
+  description: "get member count",
+  dm_permission: false, // ensures that the command cannot be used inside of dms
+  default_member_permissions: 0, // u can use default member permission to lock cmds to certain permission levels, ex administrator, u can use permissionbitfield to get one if u cant via discord docs
+};
+/**
+ *
+ * @param {import("discord.js").ChatInputCommandInteraction<'cached'>} interaction
+ * @param {import("../bot.js").Bot} client
+ */
+export async function execute(interaction, client) {
+  await interaction.deferReply({ ephemeral: false });
+  // get member count of server
+  const memberCount = interaction.guild.memberCount;
+  /**@type {import("discord.js").APIEmbed[]} */
+  const embed = [
+    {
+      description: memberCount,
+      footer: {
+        text: "Member Count",
+      },
+      timestamp: new Date(),
+    },
+  ];
+}
