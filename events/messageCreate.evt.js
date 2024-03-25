c62466f2445fb704630c64c22acc3ecdcf18f1ca
Brncray <57870234+Brncray@users.noreply.github.com>
2024-03-24 20:56:27 -0400

    Update messageCreate.evt.js

--- events/messageCreate.evt.js
+++ events/messageCreate.evt.js
@@ -9,26 +9,15 @@ export const data = {
 export async function execute(message, client) {
     if (!message.inGuild()) return;
 
-    const prefix = "!";
-
-    if (message.content.startsWith(prefix)) {
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const cmd = args.shift().toLowerCase();
-
-        if (cmd === "embed") {
-            /**@type {import("discord.js").APIEmbed[]} */
-            const e = [
-                {
-                    color: client.settings.color,
-                    footer: { text: `${message.guild.name}`, icon_url: message.guild.iconURL({ forceStatic: true }) },
-                    title: "Test",
-                    description: "test"
-                }
-            ];
-
-            await message?.reply({ embeds: e, allowedMentions: { parse: ["users", "roles", "everyone"] }, content: "hello world" });
-                /*await client?.user?.setAvatar("https://cdn.discordapp.com/icons/1099918152592261272/2f9818deb03f5021d6f61bdc6e58dd75.png"),
-                await message.channel?.send({ content: "Successfully Changed Profile Picture" });*/
-        }
+    const regex = "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)"
+    if (message.content.match(regex)) {
+        message.delete();
+        message.channel.send({
+            content: `${message.author} you cannot send links in this server.`
+        }).then(m => {
+            setTimeout(() => {
+                m.delete();
+            }, 5000);
+        });
     }
 }
