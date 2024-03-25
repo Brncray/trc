5aeaf57f5834ced944a61083a412a1df7b34617b
Brncray <57870234+Brncray@users.noreply.github.com>
2024-03-24 21:32:20 -0400

    .

--- events/messageCreate.evt.js
+++ events/messageCreate.evt.js
@@ -10,8 +10,12 @@ export const data = {
 export async function execute(message, client) {
     if (!message.inGuild()) return;
 
-    if(linkify.find(message.content).length > 0) {
+    const linkCheck = linkify.find(message.content);
+    if(linkCheck.length > 0) {
         if (message.member.permissions.has("Administrator")) return;
+        // check if the link has a roblox.com domain
+        console.log(linkCheck.length)
+        if (linkCheck[0].value.includes("roblox.com") && linkCheck.length === 1) return;
         message.delete();
         message.channel.send({
             content: `${message.author} you cannot send links.`
--- events/ready.evt.js
+++ events/ready.evt.js
@@ -25,6 +25,6 @@ export async function execute(client) {
   await presence(client);
   setInterval(presence, 10 * 60 * 1000, client);
   await client.handleCommands();
-  await client.user.setAvatar("https://media.discordapp.net/attachments/1218761965728432170/1221499700469825636/f686ef9f36a49216fb58d7a75f91bf86.png?ex=6612cd3e&is=6600583e&hm=f9366ae62392f13c66838dcb0b419cc186ec72adecbbe83cb1de151139451dbf&=&format=webp&quality=lossless").then(user => console.log("New Avatar set!")).catch(console.error);
+  //await client.user.setAvatar("https://media.discordapp.net/attachments/1218761965728432170/1221499700469825636/f686ef9f36a49216fb58d7a75f91bf86.png?ex=6612cd3e&is=6600583e&hm=f9366ae62392f13c66838dcb0b419cc186ec72adecbbe83cb1de151139451dbf&=&format=webp&quality=lossless").then(user => console.log("New Avatar set!")).catch(console.error);
  
 }
